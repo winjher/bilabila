8a803721ce43586d9467c664ea4c7e052933fa71
winjher <138558440+winjher@users.noreply.github.com>
2025-03-20 13:14:21 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.title('🦋 Bilabila Machine Learning App')
 
 st.info('This is app builds a Machine Learning Model')
+
+df=pd.read_csv("https://github.com/winjher/bilabila/blob/master/Data/butterfly_data.csv")
+df
